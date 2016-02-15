cce8a12dacc1fd0ce4e0ecf05d432045c419e287
Matthew McDermott <mattmcdermott8@gmail.com>
2016-02-15 08:26:28 -0800

    Simplified test code.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,10 @@
 require 'faker'
 
+unique_auth0_id = 0
+
 FactoryGirl.define do
   factory :user do |f|
     f.username { Faker::Internet.user_name }
-    f.auth0_id "0"
+    f.auth0_id { "#{unique_auth0_id += 1}" }
   end
 end
--- spec/models/spaces_spec.rb
+++ spec/models/spaces_spec.rb
@@ -2,29 +2,20 @@ require 'rails_helper'
 
 RSpec.describe Space, type: :model do
   it "Can't be created privately on users without private access" do
-    user = FactoryGirl.create(:user)
-    user.has_private_access = false
-    user.save
+    user = FactoryGirl.create :user
     expect(user.spaces.create(is_private: true).save).to eq false
   end
 
   it "Can be created privately on users with private access" do
-    user = FactoryGirl.create(:user)
-    user.has_private_access = true
-    user.private_access_count = 3
-    user.save
+    user = FactoryGirl.create :user, has_private_access: true, private_access_count: 3
     expect(user.spaces.create(is_private: true).save).to eq true
   end
 
   it "Can be created publicly on any user" do
-    user = FactoryGirl.create(:user)
-    user.save
+    user = FactoryGirl.create :user
     expect(user.spaces.create().save).to eq true
 
-    user = FactoryGirl.create(:user)
-    user.has_private_access = true
-    user.private_access_count = 3
-    user.save
+    user = FactoryGirl.create :user, has_private_access: true, private_access_count: 3
     expect(user.spaces.create().save).to eq true
   end
 
